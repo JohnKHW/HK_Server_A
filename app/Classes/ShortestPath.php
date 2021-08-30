af1eef1879280de03c2e210e9b53fb2a39be1c69
JohnKHW <wongkahingerv@gmail.com>
2021-08-30 21:03:27 +0800

    Algo

--- app/Classes/ShortestPath.php
+++ app/Classes/ShortestPath.php
@@ -9,27 +9,61 @@ class ShortestPath
 {
     public static function findShortest(Place $place, $depTime, $target, $set, &$result, $time = 0, $route = [])
     {
+        $color = [];
+        $color[$place->id] = 0;
+        $queue = [[$place, $depTime]];
+        $stepNode = [[$place->id]];
 
-        $schedules = $place
+        $bfsStep = 1;
+        $tempTotal = 1;
+        $tempEnd = 0;
+        while ($queue != null) {
+            // error_log(json_encode($color));
+            $fromPlace = array_pop($queue)[0];
+            $pathList = ShortestPath::getSchedules($fromPlace, $depTime);
+            $tempTotal = sizeof($pathList);
+            // error_log('tempTotal: ' . $tempTotal);
+
+            for ($i = 0; $i < $tempTotal; $i++) {
+                $newPlace = [$pathList[$i]->toPlace, $pathList[$i]->arrival_time];
+                if (array_key_exists($newPlace[0]->id, $color)) {
+                    continue;
+                }
+
+                array_push($queue, $newPlace);
+                if ($newPlace[0]->id != $target) {
+                    $color[$newPlace[0]->id] = $bfsStep;
+                }
+                $tempStepNode = [];
+                error_log($i . ' ' . sizeof($stepNode[$bfsStep - 1]));
+                for ($j = 0; $j < sizeof($stepNode[$bfsStep - 1]); $j++) {
+                    // error_log(end($stepNode[$bfsStep - 1]));
+                    // error_log($fromPlace->id);
+                    // error_log('aaa ' . (end($stepNode[$bfsStep - 1]) == $fromPlace->id));
+                    $endNode = $stepNode[$bfsStep - 1];
+                    if (end($endNode) == $fromPlace->id) {
+                        array_push($endNode, $newPlace[0]->id);
+                        array_push($tempStepNode, $endNode);
+                        //error_log('tempNode ' . json_encode($endNode));
+                    }
+                }
+                // error_log('New');
+                $stepNode[$bfsStep] = $tempStepNode;
+                $tempEnd = $newPlace[0]->id;
+                // error_log(json_encode($stepNode));
+            }
+            if ($fromPlace->id == $tempEnd) {
+                $bfsStep++;
+            }
+        }
+        // error_log(json_encode($color));
+    }
+
+    public static function getSchedules(Place $depPlace, $depTime)
+    {
+        return $depPlace
             ->fromSchedules()
             ->where('departure_time', '>=', $depTime)
             ->get();
-        for ($i = 0; $i < sizeof($schedules); $i++) {
-            error_log('schedules id: ' . $schedules[$i]->id);
-            $newRoute = $route;
-            array_push($newRoute, $schedules[$i]->id);
-            $departure_time = strtotime($schedules[$i]->departure_time);
-            $arrival_time = strtotime($schedules[$i]->arrival_time);
-            $newTime = $time + date('i', $arrival_time - $departure_time);
-            if ($schedules[$i]->to_place_id === $target) {
-                array_push($result, [$newRoute, $newTime]);
-                continue;
-            } else if (in_array($schedules[$i]->to_place_id, $set)) {
-                continue;
-            } else {
-                array_push($set, $schedules[$i]->to_place_id);
-                ShortestPath::findShortest($schedules[$i]->toPlace()->first(), $schedules[$i]->arrival_time, $target, $set, $result, $newTime, $newRoute);
-            }
-        }
     }
 }
--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Classes\ShortestPath;
 use App\Models\Place;
 use App\Models\Route;
+use App\Models\Schedule;
 use Illuminate\Http\Request;
+use SebastianBergmann\Environment\Console;
 
 class RouteController extends Controller
 {
@@ -20,7 +22,33 @@ class RouteController extends Controller
         // for ($i = 0; $i < sizeof($schedules); $i++) {
         //     ShortestPath::findShortest($schedules[$i]->toPlace()->first(), $schedules[$i]->arrival_time, $toPlace->id, $set, $result);
         // }
-        ShortestPath::findShortest($fromPlace, $depTime, $toPlace->id, $set, $result);
+        $schedules = Schedule::all();
+        $edges = [];
+        foreach ($schedules as $schedule) {
+            //$deltaTime = strtotime($schedule->arrival_time) - strtotime($schedule->departure_time);
+            array_push($edges[$schedule->from_place_id], $schedule->to_place_id);
+        }
+        $paths = [];
+        $color = [];
+        $queue = [$fromPlace->id];
+        while ($queue != null) {
+            $tempPath = [];
+            $firstSource = array_pop($queue);
+            if (in_array($firstSource, $color)) {
+                continue;
+            }
+            array_push($color, $firstSource);
+            foreach ($edges as $edge) {
+                if ($edge['s'] == $firstSource) {
+                    array_push($tempPath, $edge['d']);
+                    array_push($queue, $edge['d']);
+                }
+            }
+        }
+
+
+        error_log(json_encode($paths));
+        //ShortestPath::findShortest($fromPlace, $depTime, $toPlace->id, $set, $result);
         $result = array_unique($result, SORT_REGULAR);
         sort($result);
         return response([
